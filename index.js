92a4448307b02f24e5636041b18c052e59a927c9
kschuma <kschuma@gmail.com>
2017-09-25 20:29:20 -0700

    Tweaks to iterate over svg nodes in IE and Edge.

--- index.js
+++ index.js
@@ -3,22 +3,18 @@ const cloneElement = el => {
   // create a copy of the element
   //
   const clone = el.cloneNode(false);
-  if (el.children === undefined || el.children.length === 0) {
-    clone.innerHTML = el.innerHTML;
-  } else {
-    [ ...el.children ].forEach(child => {
-      clone.appendChild(cloneElement(child));
-    });
-  }
+  const children = [ ...el.childNodes ];
+  children.forEach(child => {
+    clone.appendChild(cloneElement(child));
+  });
 
-  // apply styles
-  //
-  const source = window.getComputedStyle(el);
-  const target = clone.style;
+  if (el.nodeType === Node.ELEMENT_NODE) {
 
-  if (source.cssText)
-    target.cssText = source.cssText;
-  else {
+    // apply styles
+    //
+    const source = window.getComputedStyle(el);
+    const target = clone.style;
+
     [ ...source ].forEach(name =>
       target.setProperty(name,
         source.getPropertyValue(name),
@@ -26,8 +22,18 @@ const cloneElement = el => {
       )
     );
   }
+
   return clone;
 };
+
+const toHack = el => Promise.reject({
+  message: `
+    Browser does support the foreignObject tag.
+
+    If you see this error, please consult https://github.com/kyleschuma/to-png-foh#readme for a workaround.
+  `,
+  svg: toSvg(el)
+});
 
 const toCanvas = (el, scale=1) =>
   toImage(el).then(image => {
@@ -43,27 +49,26 @@ const toCanvas = (el, scale=1) =>
   });
 
 const toImage = el => new Promise((resolve, reject) => {
-  const html = new XMLSerializer()
-    .serializeToString(cloneElement(el))
-    .replace(/#/g, '%23')
-    .replace(/\n/g, '%0A');
-  const uri = `
-    data:image/svg+xml;charset=utf-8,
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="${el.offsetWidth}" height="${el.offsetHeight}">
-
-      <foreignObject x="0" y="0" width="100%" height="100%">
-        ${html}
-      </foreignObject>
-    </svg>
-  `;
   const image = new Image();
   image.onload = () => resolve(image);
   image.onerror = reject;
-  image.src = uri;
+  image.src = `data:image/svg+xml;charset=utf-8,${toSvg(el)}`;
 });
 
+const toSvg = el => `
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="${el.offsetWidth}" height="${el.offsetHeight}">
+    <foreignObject x="0" y="0" width="100%" height="100%">
+      ${new XMLSerializer().serializeToString(cloneElement(el))}
+    </foreignObject>
+  </svg>
+`;
+
+const supportsForeignObjects = () =>
+  document.implementation.hasFeature('http://www.w3.org/TR/SVG11/feature#Extensibility', '1.1');
+
 export default (...args) =>
-  toCanvas(...args)
-    .then(canvas => canvas.toDataURL());
+  supportsForeignObjects() ?
+    toCanvas(...args).then(canvas => canvas.toDataURL()) :
+    toHack(...args);
