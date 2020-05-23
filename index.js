cba0644f4ce517402d652189835aa797207ebea6
kyle <kyle@shrug.local>
2020-05-22 18:33:30 -0600

    Ran index.js through prettier. Not sure if it looks any better though.

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
-
 const cloneElement = el => {
-
   const clone = el.cloneNode(false);
-  const cloneChildren = () => Promise.all(
-    [ ...el.childNodes ].map(child => cloneElement(child))
-  );
-  const copyCSS = () => new Promise(accept => {
-
-    setTimeout(() => {
-      // using timeout is a hack to prevent blocking of ui interaction for more
-      // information about timeout and blocking see:
-      // https://johnresig.com/blog/how-javascript-timers-work/#postcomment
-      //
-      if (el.nodeType === Node.ELEMENT_NODE) {
-
-        // apply styles
+  const cloneChildren = () => Promise.all([...el.childNodes].map(child => cloneElement(child)));
+  const copyCSS = () =>
+    new Promise(accept => {
+      setTimeout(() => {
+        // using timeout is a hack to prevent blocking of ui interaction for more
+        // information about timeout and blocking see:
+        // https://johnresig.com/blog/how-javascript-timers-work/#postcomment
         //
-        const source = window.getComputedStyle(el);
-        const target = clone.style;
-        const styles = [ ...source ];
-        styles.forEach(name =>
-          target.setProperty(name, source.getPropertyValue(name))
-        );
-      }
-      accept();
-    }, 10);
-  });
+        if (el.nodeType === Node.ELEMENT_NODE) {
+          // apply styles
+          //
+          const source = window.getComputedStyle(el);
+          const target = clone.style;
+          const styles = [...source];
+          styles.forEach(name => target.setProperty(name, source.getPropertyValue(name)));
+        }
+        accept();
+      }, 10);
+    });
 
   return Promise.resolve()
     .then(copyCSS)
@@ -36,21 +29,22 @@ const cloneElement = el => {
     });
 };
 
-const toWorkAround = el => new Promise((accept, reject) =>
-  toSvg(el).then(svg =>
-    reject({
-      foreignObjectsRequired: true,
-      message: `
+const toWorkAround = el =>
+  new Promise((accept, reject) =>
+    toSvg(el).then(svg =>
+      reject({
+        foreignObjectsRequired: true,
+        message: `
         Browser does support the foreignObject tag.
 
         If you see this error, please consult https://github.com/kyleschuma/to-png#readme for a possible workaround.
       `,
-      svg,
-    })
-  )
-);
+        svg,
+      })
+    )
+  );
 
-const toCanvas = (el, scale=1) =>
+const toCanvas = (el, scale = 1) =>
   toImage(el).then(image => {
     const canvas = document.createElement('canvas');
     const context = canvas.getContext('2d');
@@ -63,36 +57,43 @@ const toCanvas = (el, scale=1) =>
     return canvas;
   });
 
-const toImage = el => toSvg(el).then(svg => new Promise((resolve, reject) => {
+const toImage = el =>
+  toSvg(el).then(
+    svg =>
+      new Promise((resolve, reject) => {
+        const src = `data:image/svg+xml;charset=utf-8,${encodeURIComponent(svg)}`;
+        const image = new Image();
+        image.onload = () => resolve(image);
+        image.onerror = err => reject(err);
+        image.src = src;
+      })
+  );
 
-  const src = `data:image/svg+xml;charset=utf-8,${encodeURIComponent(svg)}`;
-  const image = new Image();
-  image.onload = () => resolve(image);
-  image.onerror = err => reject(err);
-  image.src = src;
-}));
-
-const toSvg = el => cloneElement(el).then(clone => `
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="${el.offsetWidth}" height="${el.offsetHeight}">
-    <foreignObject x="0" y="0" width="100%" height="100%">
-      ${new XMLSerializer().serializeToString(clone)}
-    </foreignObject>
-  </svg>
-`);
+const toSvg = el =>
+  cloneElement(el).then(
+    clone => `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="${el.offsetWidth}" height="${el.offsetHeight}">
+        <foreignObject x="0" y="0" width="100%" height="100%">
+          ${new XMLSerializer().serializeToString(clone)}
+        </foreignObject>
+      </svg>
+    `
+  );
 
 const supportsForeignObjects = () =>
   document.implementation.hasFeature('http://www.w3.org/TR/SVG11/feature#Extensibility', '1.1');
 
 const toResponse = canvas =>
-  new Promise(resolve => canvas.toBlob(blob =>
-    resolve({
-      blob,
-      dataUrl: canvas.toDataURL(),
-    })
-  ));
+  new Promise(resolve =>
+    canvas.toBlob(blob =>
+      resolve({
+        blob,
+        dataUrl: canvas.toDataURL(),
+      })
+    )
+  );
 
-export default (...args) => supportsForeignObjects() ?
-  toCanvas(...args).then(toResponse) :
-  toWorkAround(...args);
+export default (...args) =>
+  supportsForeignObjects() ? toCanvas(...args).then(toResponse) : toWorkAround(...args);
